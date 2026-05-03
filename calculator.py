1bf6755412304902082f52edf7582388ca15414a
aytgn <aytgnozgn@gmail.com>
2026-05-03 14:35:09 +0300

    Test düzeltildi, 4 işlem eklendi

--- calculator.py
+++ calculator.py
@@ -1,3 +1,14 @@
 # calculator.py
 def topla(a, b):
     return a + b
+
+def cikar(a, b):
+    return a - b
+
+def carp(a, b):
+    return a * b
+
+def bol(a, b):
+    if b == 0:
+        raise ValueError("Bölen sıfır olamaz.")
+    return a / b
